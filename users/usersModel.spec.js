c829461a890cd23607beb41ba5acc15ead062782
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-07-26 12:30:50 -0500

    added some testing endpoints

--- users/usersModel.spec.js
+++ users/usersModel.spec.js
@@ -14,6 +14,7 @@ describe("users model", () => {
     expect(process.env.DB_ENV).toBe("testing");
   });
 
+  //// TWO POST REQUESTS FOR REGISTER
   describe("add()", () => {
     it("should insert user into db", async () => {
       await Users.add({ username: "chris", password: "goalie" });
@@ -21,10 +22,35 @@ describe("users model", () => {
 
       expect(users).toHaveLength(1);
     });
-    it("should insert user into db", async () => {
+
+    it("should provide username of chris", async () => {
       const users = await Users.add({ username: "chris", password: "goalie" });
 
       expect(users.username).toBe("chris");
+    });
+  });
+
+  //// 1 GET REQUEST
+  describe("get dad jokes", () => {
+    it("should return 404 for unauthorized request", () => {
+      return request(server)
+        .post("/jokes")
+        .set("Authorization", "incorrectToken")
+        .then(res => {
+          expect(res.status).toBe(404);
+        });
+    });
+  });
+
+  //// LOGIN REQUESTS
+  describe("add()", () => {
+    it("should insert user into db", async () => {
+      await Users.add({ username: "chris", password: "goalie" });
+      const users = await db("users");
+      await request(server)
+        .post('/api/login')
+        .send({ username: "chris", password: "goalie" })
+        .expect(401)
     });
   });
 });
