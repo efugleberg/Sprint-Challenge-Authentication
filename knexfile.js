7b291fbdbfa77160a32da085f39df397d980db5b
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-07-26 11:20:34 -0500

    all routes functioning properly

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,18 @@
 
 module.exports = {
   development: {
-    client: 'sqlite3',
-    connection: { filename: './database/auth.db3' }, // change this if you want a different name for the database
+    client: "sqlite3",
+    connection: { filename: "./database/auth.db3" }, // change this if you want a different name for the database
     useNullAsDefault: true, // used to avoid warning on console
     migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
+      directory: "./database/migrations",
+      tableName: "dbmigrations"
     },
-    seeds: { directory: './database/seeds' },
-  },
+    pools: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
+    seeds: { directory: "./database/seeds" }
+  }
 };
